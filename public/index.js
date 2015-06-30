3f697e35ffee63a06664d3d080d1ffb55482f920
goediaz <Rodrigo@Godis-MacBook.local>
2015-06-29 21:46:44 -0300

    New feature

---
+++ public/index.js
@@ -0,0 +1,20 @@
+//Formulatio de Validación
+
+function validateForm() {
+        var username = document.getElementById(/*"username"*/).value;
+        var password = document.getElementById(/*"password"*/).value;
+        if (username == null || username == "") {
+            alert("Por favor, poné tu Nombre de Usuario.");
+            return false;
+        }
+        if (password == null || password == "") {
+            alert("Porque favor, poné tu contraseña.");
+            return false;
+        }
+        alert("Conectado");
+        onSuccsess(client);
+    } 
+
+//function onSuccsess(client){
+//	$('')
+//}
