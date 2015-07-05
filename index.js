2c114ce69408fb26fb9c568cdc20bd1b0fa54d0d
godiaz <godi@Godi-MacBook-Air.local>
2015-07-05 03:16:24 -0300

    New Server and json

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 app.use(express.static('public'));
 
-
 app.get('/getuser', function (req, res) {
-  res.send("user");
+  res.setHeader('Content-Type', 'application/json');
+  fs.readFile('./data/usuarios.json', function (err, data) {
+	     if (err){ throw err; }
+	        console.log("INFO: sending usuarios data... ");
+		    console.dir(JSON.parse(data.toString()));
+		    res.send(data.toString());
+  });
 });
 
 app.get('/getproduct', function (req, res) {
-	res.send("producto");
+	res.setHeader('Content-Type', 'application/json');
+
+	fs.readFile('./data/productos.json', function (err, data) {
+	     if (err){ throw err; }
+	        console.log("INFO: sending productos data... ");
+		    console.dir(JSON.parse(data.toString()));
+		    res.send(data.toString());
+	});
 });
 
-app.post('/getcomprasbyuser', function (req, res) {
-	res.send("compra");
+app.post('/getcompras', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	fs.readFile('./data/compras.json', function (err, data) {
+		  if (err){ throw err; }
+	        console.log("INFO: sending compras data... ");
+		    console.dir(JSON.parse(data.toString()));
+		    res.send(data.toString());
+	});
 });
 
 app.post('/addproductcart', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	console.log("INFO: sending product cart data... ");
 	res.send("cart");
 });
 
@@ -25,7 +46,7 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Server UP', host, port);
+  console.log('INIT: Server UP on ' +  host + ":"+ port);
 
 });
 
