875e37831d23f2a87b0a8e2473743f86388a7b42
unknown <gastonsitoula@gmail.com>
2015-07-05 00:25:10 -0300

    Adding end points

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ var app = express();
 
 app.use(express.static('public'));
 
+//
+app.get('/getuser', function (req, res) {
+  res.send("user");
+});
+
+app.get('/getproduct', function (req, res) {
+	res.send("producto");
+});
+
+app.post('/getcomprasbyuser', function (req, res) {
+	res.send("compra");
+});
+
+app.post('/addproductcart', function (req, res) {
+	res.send("cart");
+});
+
 var server = app.listen(3000, function () {
 
   var host = server.address().address;
@@ -11,3 +28,5 @@ var server = app.listen(3000, function () {
   console.log('Server UP', host, port);
 
 });
+
+
