87660f79414fc6b541e0351bf54e275a2e56f577
unknown <gastonsitoula@gmail.com>
2015-07-07 20:31:52 -0300

    new changes

--- public/index.js
+++ public/index.js
@@ -14,17 +14,11 @@ function produkts(callback) {
             $elem = $($elem);
 
             var $button = $('<div>' + '<button ' + ' id="' + data._id + '" ' + '> Agregar al Carrito </button>').click(function(e) {
-
+                  
+                console.log(data);
                 $.getJSON( "/getUserCart", function( data ) {
-                var items = [];
-                $.each( data, function( key, val ) {
-                items.push( "<li id='" + key + "'>" + val + "</li>" );
-                });
- 
-                $( "<ul/>", {
-                "class": "my-new-list",
-                html: items.join( "" )
-                }).appendTo( "body" );
+                        console.log(data);
+                        
             });
 
 
@@ -45,37 +39,37 @@ function produkts(callback) {
 var rat = [];
 
 
-function getUserCart(callback) {
+// function getUserCart(callback) {
 
-    $.getJSON("/getUserCart", function(result) {
-        var $html = $('<ul class="cart-list"></ul>');
-
-
-        result.Cart.forEach(function(data) {
-
-            var $elem = '<li class="cart-list-item"' +
-                'id="' + data._id + '">' +
-                '<div>' + data.name + '</div>'
-            '</li>';
-
-            $elem = $($elem);
+//     $.getJSON("/getUserCart", function(result) {
+//         var $html = $('<ul class="cart-list"></ul>');
 
 
-            var $button = $('<div>' + '<button ' + ' id="' + data._id + '" ' + '> Eliminar </button>').click(function(e) {
-                console.log(rat);
+//         result.Cart.forEach(function(data) {
+
+//             var $elem = '<li class="cart-list-item"' +
+//                 'id="' + data._id + '">' +
+//                 '<div>' + data.name + '</div>'
+//             '</li>';
+
+//             $elem = $($elem);
 
 
-            });
+//             var $button = $('<div>' + '<button ' + ' id="' + data._id + '" ' + '> Eliminar </button>').click(function(e) {
+//                 console.log(rat);
 
-            $elem.append($button);
-            $html.append($elem);
-        });
 
-        callback($html);
+//             });
 
-    });
+//             $elem.append($button);
+//             $html.append($elem);
+//         });
 
-};
+//         callback($html);
+
+//     });
+
+// };
 
 
 $(document).ready(function() {
@@ -83,8 +77,8 @@ $(document).ready(function() {
     produkts(function(dom) {
         $('.products-module').append(dom);
     });
-    getUserCart(function(dom) {
-        $('.cart-module').append(dom);
-    });
+    // getUserCart(function(dom) {
+    //     $('.cart-module').append(dom);
+    // });
 
 });
